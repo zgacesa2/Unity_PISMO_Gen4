cd3aca5ad1572f425d36b6f7169a89835417451e
Cwekii <dominik.cvetkovski@gmail.com>
2020-07-20 11:41:24 +0200

    Rigidbody zadatci 1

---
+++ Programiranje/02_Rigidbody/Zadatak4.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Skripta za zaključavanje rotacije po z osi, te kretanja po Y osi
+
+public class Zadatak4 : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationZ;
+        rb.constraints = RigidbodyConstraints.FreezePositionX;
+    }
+}
