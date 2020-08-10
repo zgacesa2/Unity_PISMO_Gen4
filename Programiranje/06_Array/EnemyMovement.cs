ca3a34a8adbe00575c09b3ce16ac2c7f5d72a846
Cwekii <dominik.cvetkovski@gmail.com>
2020-08-10 12:47:08 +0200

    Array Spawning Enemy

---
+++ Programiranje/06_Array/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public GameObject target; //to smo mi i ona ide prema nama
+    public float speed;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        transform.LookAt(target.transform);
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+    }
+}
