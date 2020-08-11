8290fc248e80ece323875909d74525584405fe32
Cwekii <dominik.cvetkovski@gmail.com>
2020-08-11 12:22:43 +0200

    Update enemy

--- Programiranje/06_Array/EnemyMovement.cs
+++ Programiranje/06_Array/EnemyMovement.cs
@@ -6,6 +6,7 @@ public class EnemyMovement : MonoBehaviour
 {
     public GameObject target; //to smo mi i ona ide prema nama
     public float speed;
+    public float dmg;
 
     private void Start()
     {
@@ -16,5 +17,15 @@ public class EnemyMovement : MonoBehaviour
     {
         transform.LookAt(target.transform);
         transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Health hp = target.GetComponent<Health>();
+            hp.currentHealth -= dmg;
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ Programiranje/Health.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Text healthText; //Prikaz healtha na UI
+    public float currentHealth; // Trenutni health
+    public float maxHealth = 100f; //Maximalni i pocetni health
+    [Header("Regeneraciju upisite u iznosu regeneracije svake sekunde")]
+    public float healthRegen; //Regeneracija healtha
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthText.text = (int)currentHealth + "/" + (int)maxHealth;
+    }
+
+    private void Update()
+    {
+        //Regeneracija
+        if(currentHealth < maxHealth && currentHealth > 0)
+        {
+            currentHealth += healthRegen * Time.deltaTime;
+        }
+        if(currentHealth <= 0)
+        {
+            //YOU DIED MADAFAKA
+        }
+        //DEBUG NAREDBA
+        if(Input.GetKeyDown(KeyCode.U))
+        {
+            currentHealth -= 20;
+        }
+        healthText.text = (int)currentHealth + "/" + (int)maxHealth;
+    }
+}
